2940cbfea0fe8d5556eb1f136da10fb261991d12
Braxton Allen <braxtonallen99@yahoo.com>
2021-11-09 21:00:21 -0600

    finished project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,68 @@
 // TODO: Declare any global variables we need
-
+let totalHeads= 0
+let totalTails= 0
+let totalFlipped= 0
+let headsPercentage= 0
+let tailsPercentage= 0
+//let images= ["assets/images/penny-heads.jpg", "assets\images\penny-tails.jpg"]
+let images= [
+    {
+        imagePath: "assets/images/penny-heads.jpg",
+        message: "You flipped heads!",
+        side: "heads"
+    },
+    {
+        imagePath: "assets/images/penny-tails.jpg",
+        message: "You flipped tails!",
+        side: "tails"
+    }
+]
+let defaultMessage = "Let's get rolling!"
 
 document.addEventListener('DOMContentLoaded', function () {
+    
+    document.getElementById("flip").addEventListener("click", function() {
+        let randomImage = images[Math.floor(Math.random() * images.length)]
+        
+        document.querySelector("#image").setAttribute("src", randomImage.imagePath)
+
+        if(randomImage.side === "heads"){
+            totalHeads++
+            totalFlipped++
+
+            document.getElementById("heads").textContent = totalHeads
+            document.querySelector(".message-container").textContent = randomImage.message
+        } else {
+            totalTails++
+            totalFlipped++
+
+            document.getElementById("tails").textContent = totalTails
+            document.querySelector(".message-container").textContent = randomImage.message
+        }
+
+        headsPercentage = Math.round(totalHeads * 100/totalFlipped)
+        tailsPercentage = Math.round(totalTails * 100/totalFlipped)
+
+        document.getElementById("heads-percent").textContent = headsPercentage + "%"
+        document.getElementById("tails-percent").textContent = tailsPercentage + "%"
+    })
+
+    document.getElementById("clear").addEventListener("click", function(){
+        totalHeads = 0
+        totalTails = 0
+        tailsPercentage = 0 + "%"
+        headsPercentage = 0 + "%"
+        defaultMessage = "Let's get rolling!"
+
+        document.getElementById("heads").textContent = 0
+        document.getElementById("tails").textContent = 0
+        document.getElementById("heads-percent").textContent = 0 + "%"
+        document.getElementById("tails-percent").textContent = 0 + "%"
+        document.querySelector(".message-container").textContent = defaultMessage
+    })
+
     // This is just a sanity check to make sure your JavaScript script is getting loaded
     // You can remove it once you see it in your browser console in the developer tools
     console.log('Hi')
-
-    // TODO: Add event listener and handler for flip and clear buttons
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
 
 })
